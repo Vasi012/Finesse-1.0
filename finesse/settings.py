e6115eaa96d8162eeedb52c34639c225b805f30a
Vasilica Pavaloi <vp.catalin02@yahoo.com>
2022-10-15 21:13:59 +0000

    deploy to heroku

--- finesse/settings.py
+++ finesse/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEVELOPMENT')
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
@@ -190,7 +190,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
