14f599a048d7947af960aacfba187090931b95fe
Vasilica Pavaloi <vp.catalin02@yahoo.com>
2022-10-15 17:05:14 +0000

    deploy commit

--- finesse/settings.py
+++ finesse/settings.py
@@ -31,6 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['finesse-events.herokuapp.com', 'localhost']
 
 # Application definition
